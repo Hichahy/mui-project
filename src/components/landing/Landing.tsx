296cf8d67d6c97232189aa5f382f7ea516b4b505
Staszek <stasiu94pro@icload.com>
2022-06-10 18:40:06 +0200

    fix:navbar

--- src/components/landing/Landing.tsx
+++ src/components/landing/Landing.tsx
@@ -1,6 +1,7 @@
 import React, { useEffect } from "react";
 import { IMeal } from "../../types/types";
 
+
 interface IProps {
   meal: IMeal[];
   loadMeal: () => void;
--- src/components/navigation/Navigation.tsx
+++ src/components/navigation/Navigation.tsx
@@ -1,30 +1,29 @@
-import React from 'react';
+import React from "react";
 import AppBar from "@mui/material/AppBar";
 import Box from "@mui/material/Box";
 import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
-import IconButton from "@mui/material/IconButton";
-import MenuIcon from "@mui/icons-material/Menu";
+import FastfoodOutlinedIcon from "@mui/icons-material/FastfoodOutlined";
 
 const Navigation = () => {
   return (
-    <Box sx={{ flexGrow: 1 }}>
-      <AppBar position="static">
-        <Toolbar variant="dense">
-          <IconButton
-            edge="start"
-            color="inherit"
-            aria-label="menu"
-            sx={{ mr: 2 }}
-          >
-            <MenuIcon />
-          </IconButton>
-          <Typography variant="h6" color="inherit" component="div">
-            NetFoodTrack
-          </Typography>
-        </Toolbar>
-      </AppBar>
-    </Box>
+    <>
+      <Box sx={{ flexGrow: 1 }}>
+        <AppBar position="static">
+          <Toolbar variant="dense">
+            <Typography
+              display="flex"
+              alignItems="center"
+              variant="h6"
+              color="inherit"
+              component="div"
+            >
+              <FastfoodOutlinedIcon /> FeedMe
+            </Typography>
+          </Toolbar>
+        </AppBar>
+      </Box>
+    </>
   );
 };
 
