e9666b1392ad75c9da7232114df5ff1442a84545
Staszek <stasiu94pro@icload.com>
2022-06-11 16:16:36 +0200

    feat:add cards to meal

--- src/components/landing/Landing.tsx
+++ src/components/landing/Landing.tsx
@@ -1,6 +1,11 @@
 import React, { useEffect } from "react";
+import "./landing.scss"
 import { IMeal } from "../../types/types";
-
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Typography from "@mui/material/Typography";
+import { CardActionArea } from "@mui/material";
 
 interface IProps {
   meal: IMeal[];
@@ -17,9 +22,26 @@ const Landing = ({ meal, loadMeal }: IProps) => {
   }
 
   return (
-    <div>
+    <div className="container-card">
       {meal.map((i) => (
-        <p key={i.id}>{i.name}</p>
+        <Card style={{margin: "20px"}} key={i.id} sx={{ maxWidth: 345 }}>
+          <CardActionArea>
+            <CardMedia
+              component="img"
+              height="240"
+              image={i.image}
+              alt={i.name}
+            />
+            <CardContent>
+              <Typography gutterBottom variant="h5" component="div">
+                {i.name}
+              </Typography>
+              <Typography variant="body2" color="text.secondary">
+                {i.description}
+              </Typography>
+            </CardContent>
+          </CardActionArea>
+        </Card>
       ))}
     </div>
   );
